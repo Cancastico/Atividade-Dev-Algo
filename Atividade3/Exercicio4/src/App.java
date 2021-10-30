a666cd3cb14c2f6863b2f1a78c141806c419b3c4
Carlos Augusto <carlosaaugusto2002@outlook.com>
2021-10-29 22:24:06 -0300

    Atividade3-Exercicio4

--- Atividade3/Exercicio4/src/App.java
+++ Atividade3/Exercicio4/src/App.java
@@ -1,5 +1,21 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String numero1, numero2;
+        int numero1Int, numero2Int;
+
+        numero1 = JOptionPane.showInputDialog(null, "Digite o primeiro numero");
+        numero1Int = Integer.parseInt(numero1);
+        numero2 = JOptionPane.showInputDialog(null, "Digite o segundo numero ");
+        numero2Int = Integer.parseInt(numero2);
+
+        if(numero1Int > numero2Int ){
+            JOptionPane.showMessageDialog(null, "O numero " + numero1Int + " é maior");
+        }else if(numero1Int < numero2Int ){
+            JOptionPane.showMessageDialog(null, "O numero " + numero2Int + " é maior");
+        }else{
+            JOptionPane.showMessageDialog(null, "Os numeros são iguais");
+        }
     }
 }
