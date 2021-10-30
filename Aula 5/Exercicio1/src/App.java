3efce8eee3d5b285645f1e0a1d6eda49f71d6841
Carlos Augusto <carlosaaugusto2002@outlook.com>
2021-10-30 19:49:28 -0300

    Aula 5  Exercicio 1 Completo

--- Aula 5/Exercicio1/src/App.java
+++ Aula 5/Exercicio1/src/App.java
@@ -1,10 +1,15 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
         String valor, codigo;
         int  codigoInt;
-        double valorDouble, desconto;
+        double valorDouble;
+        DecimalFormat df = new DecimalFormat("#.00");
+        DecimalFormat dfUP = new DecimalFormat("#.00");
+                        dfUP.setRoundingMode(RoundingMode.UP);
         valor = JOptionPane.showInputDialog(null, "Digite o valor do produto");
         valorDouble = Double.parseDouble(valor);
         codigo = JOptionPane.showInputDialog(null, "Escolha uma Opção de pagamento \n 1, A vista no dinheiro, 10% de desconto \n 2, A vista no cartão de crédito \n 3, 2x no cartão de crédito, sem desconto");
@@ -12,10 +17,13 @@ public class App {
         switch(codigoInt){
             case 1:
                 JOptionPane.showMessageDialog(null, "O Valor do produto é:"+ valorDouble + " \n O Valor a Ser pago é: " + (valorDouble * 0.9));  
-            break
+            break;
             case 2:
                 JOptionPane.showMessageDialog(null, "O Valor do produto é:"+ valorDouble + " \n O Valor a Ser pago é: " + (valorDouble * 0.98));  
-            break
+            break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "As parcelos terão o valor de :\n 1º Parcela : " + dfUP.format(valorDouble/2) +
+                                                     "\n 2º Parcela: " + df.format(valorDouble/2));
         }
     }
 }
