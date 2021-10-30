53ad334f30c02a0fb10947b76e0d3ad97df15d0d
Carlos Augusto <carlosaaugusto2002@outlook.com>
2021-10-29 22:29:53 -0300

    Exercicio 5

--- Atividade3/Exercicio5/src/App.java
+++ Atividade3/Exercicio5/src/App.java
@@ -1,5 +1,35 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String numero1, numero2;
+        int numero1Int, numero2Int;
+
+        numero1 = JOptionPane.showInputDialog(null, "Digite o primeiro numero");
+        numero1Int = Integer.parseInt(numero1);
+        numero2 = JOptionPane.showInputDialog(null, "Digite o segundo numero ");
+        numero2Int = Integer.parseInt(numero2);
+
+        if(numero1Int > numero2Int ){
+            
+            if(numero1Int % 2 == 0){
+                JOptionPane.showMessageDialog(null, "O numero " + numero1Int + " é maior e é Par");
+            }else{
+                JOptionPane.showMessageDialog(null, "O numero " + numero1Int + " é maior e é Impar");
+            }
+
+        }else if(numero1Int < numero2Int ){
+            if(numero2Int % 2 == 0){
+                JOptionPane.showMessageDialog(null, "O numero " + numero2Int + " é maior e é Par");
+            }else{
+                JOptionPane.showMessageDialog(null, "O numero " + numero2Int + " é maior e é Impar");
+            }
+        }else{
+            if(numero1Int % 2 == 0){
+                JOptionPane.showMessageDialog(null, "Os Dois são pares");
+            }else{
+                JOptionPane.showMessageDialog(null, "Os dois são Impares");
+            }            
+        }
     }
 }
