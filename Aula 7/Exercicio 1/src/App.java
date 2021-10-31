c58a356c971b7d8189f408ea6a26a84d5ebcbf93
Carlos Augusto <carlosaaugusto2002@outlook.com>
2021-10-31 20:27:06 -0300

    Aula 7 - exercicio incompleto

---
+++ Aula 7/Exercicio 1/src/App.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("\n\n\n\n");
+
+        String[] jogos = {"Candy Crush", "Super Mario", "Cut the Rope"}, cupomCod = {"SETEMBROCHOVE", "NAOCHOVENAO", "CHOVEPAKAS"};
+        double[] valores = {10.77, 23.65, 21.90};
+
+        // Bloco impressão
+        System.out.println("cód" + " \t Nome " + " \t\t valor ");
+        System.out.println("-----------------------------------");     
+        for (int i = 0; i < valores.length; i++) {
+            System.out.println(i+1 + " \t " + jogos[i] + " \t R$ " + valores[i]);           
+        }
+        System.out.println("-----------------------------------");
+
+        // Bloco entrada
+        System.out.print("\nQual jogo deseja comprar?  ");
+        Scanner entrada = new Scanner(System.in);
+        int codigoJogo = entrada.nextInt();
+
+        // Bloco esolha
+        String jogosEscolhido = jogos[codigoJogo-1];
+        double valorPagamento = valores[codigoJogo-1];  
+
+        // Bloco desconto
+        System.out.print("\n-----------------------------------");
+        System.out.print("\nVocê tem cupom de desconto ? sim(1) não(2) : "); 
+        int cupom = entrada.nextInt();
+        double desconto;
+        desconto = 0;
+
+        if (cupom == 1) {
+            System.out.print("Digite o cupom : "); 
+            String cupomCodEntrada = entrada.next().toUpperCase();
+
+            for (int i = 0; i < cupomCod.length; i++) {
+                if(cupomCodEntrada == cupomCod[i]){
+                    switch (cupomCodEntrada) {
+                        case "SETEMBROCHOVE":
+                            desconto = 0.2 * valorPagamento; // 10%
+                            System.out.println("Parabéns, você ganhou 10% de desconto");
+                            break;
+                        case "NAOCHOVENAO":
+                            desconto = 10.00; // 10%
+                            System.out.println("Parabéns, você ganhou 30% de desconto");
+                            break;
+                        case "CHOVEPAKAS":
+                            desconto = 0.5 *valorPagamento;
+                            if(desconto > 40){
+                                desconto = 40;
+                            }
+                        break;
+                        default:
+                            System.out.println("Cupom inválido");
+                            break;
+                    }
+                }else{
+                    System.out.println("Digite um cupom valido!");
+                }
+            }
+        } else {
+            System.out.print("Compra sem cupom.\n"); 
+            desconto = 0;
+        }
+        System.out.println("Jogo Escolhido: " + jogosEscolhido + "\n Valor total da compra: R$" + (valorPagamento - desconto) );
+    }
+    }
